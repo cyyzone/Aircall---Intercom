712dd4c6f71e29c7c99044a43d2ca46738c9f819
cyyzone <jenycds@hotmail.com>
2026-02-06 13:51:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ AGENTS_MAP = {
     "rhayslla.junca@produttivo.com.br": "5281911",
     "douglas.david@produttivo.com.br": "5586698",
     "aline.souza@produttivo.com.br": "5717251",
-    "bruno.braga@produttivo.com.br": "7450383",
     "heloisa.atm.slv@produttivo.com.br": "7455039",
     "danielle.ghesini@produttivo.com.br": "7628368",
     "jenyffer.souza@produttivo.com.br": "8115775",
